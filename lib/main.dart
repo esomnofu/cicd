8eb3c16795132338cf416690f5ce392f94988d4d
Mac <mac@MACs-MBP-2.lan>
2025-12-30 14:52:12 +0100

    Rf: testing

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('You have pushed the button this many times today:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
